6ed7012331e8233596589b1dea6605bdb150a8ba
Archit Rungta <architrungta120@gmail.com>
2020-02-05 03:10:41 +0530

    Create minimax_agent.py

---
+++ examples/minimax_agent.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import sys
+
+import click
+
+from gym_tictactoe.env import TicTacToeEnv, agent_by_mark, check_game_status,\
+    after_action_state, tomark, next_mark
+
+
+class HumanAgent(object):
+    def __init__(self, mark):
+        self.mark = mark
+
+    def act(self, ava_actions):
+        while True:
+            uloc = input("Enter location[1-9], q for quit: ")
+            if uloc.lower() == 'q':
+                return None
+            try:
+                action = int(uloc) - 1
+                if action not in ava_actions:
+                    raise ValueError()
+            except ValueError:
+                print("Illegal location: '{}'".format(uloc))
+            else:
+                break
+
+        return action
+
+class MinimaxAgent(object):
+    def __init__(self, mark):
+        self.mark = mark
+
+
+#   return the move in this function. ava_actions is an array containting the possible actions 
+#   you might want to use after_action_state and check_game_status. Also look at env.py
+    def act(self, ava_actions):
+        raise NotImplementedError()
+
+
+@click.command(help="Play minimax agent.")
+@click.option('-n', '--show-number', is_flag=True, default=False,
+              show_default=True, help="Show location number in the board.")
+def play(show_number):
+    env = TicTacToeEnv(show_number=show_number)
+    agents = [MinimaxAgent('O'),
+              HumanAgent('X')]
+    episode = 0
+    while True:
+        state = env.reset()
+        _, mark = state
+        done = False
+        env.render()
+        while not done:
+            agent = agent_by_mark(agents, next_mark(mark))
+            env.show_turn(True, mark)
+            ava_actions = env.available_actions()
+            action = agent.act(ava_actions)
+            if action is None:
+                sys.exit()
+
+            state, reward, done, info = env.step(action)
+
+            print('')
+            env.render()
+            if done:
+                env.show_result(True, mark, reward)
+                break
+            else:
+                _, mark = state
+        episode += 1
+
+
+if __name__ == '__main__':
+    play()
