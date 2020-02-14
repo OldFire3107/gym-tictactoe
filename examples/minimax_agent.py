a69ccd11045df5d9835b9e8f1c30dce40dc08d94
0ldf1re <agni.purani@gmail.com>
2020-02-15 01:43:12 +0530

    Minimax agent done

--- examples/minimax_agent.py
+++ examples/minimax_agent.py
@@ -31,13 +31,65 @@ class MinimaxAgent(object):
     def __init__(self, mark):
         self.mark = mark
 
+    #The actual minimax recursive function
+    def minimax(self, state, depth, isMax, ava_actions):
+        board, extra = state
+        gameState = check_game_status(board)
+
+        if gameState == 0:
+            return 0
+        elif gameState == 1:
+            return 1
+        elif gameState == 2:
+            return -1
+
+        if isMax:
+            bestScore = -100000
+            for i in range(0, 9, 1):
+                if i in ava_actions:
+                    statet = after_action_state(state, i)
+                    ava_actions.remove(i) 
+                    score = self.minimax(statet, depth + 1, False, ava_actions)
+                    ava_actions.append(i)
+                    bestScore = max(score, bestScore)
+
+            return bestScore
+        else:
+            bestScore = 100000
+            for i in range(0, 9, 1):
+                if i in ava_actions:
+                    statet = after_action_state(state, i)
+                    ava_actions.remove(i)
+                    score = self.minimax(statet, depth + 1, True, ava_actions)
+                    ava_actions.append(i)
+                    bestScore = min(score, bestScore)
+
+            return bestScore
+            
 
 #   return the move in this function. ava_actions is an array containting the possible actions 
 #   you might want to use after_action_state and check_game_status. Also look at env.py
 #   state is a tuple with the first value indicating the board and second value indicating mark
 #   proper use of inbuilt functions will avoid interacting with state
     def act(self, state, ava_actions):
-        raise NotImplementedError()
+        #raise NotImplementedError()
+        
+        bestScore = -100000
+        bestMove = 1
+
+        for i in range(0, 9, 1):
+            if i in ava_actions:
+                statet = after_action_state(state, i)
+                ava_actions.remove(i)
+                score = self.minimax(statet, 0, False, ava_actions)
+                ava_actions.append(i)
+
+                if score > bestScore:
+                    bestScore = score
+                    bestMove = i
+
+        return bestMove
+
 
 @click.command(help="Play minimax agent.")
 @click.option('-n', '--show-number', is_flag=True, default=False,
@@ -66,6 +118,9 @@ def play(show_number):
             env.render()
             if done:
                 env.show_result(True, mark, reward)
+                # board, temp = state
+                # gameState = check_game_status(board)
+                # print(gameState)
                 break
             else:
                 _, _ = state
